ccbaa89267b0008205291d8a12563dc1f0cff8ab
amandagobus <amandagobus@gmai.com>
2016-10-01 14:52:28 -0300

    arquivos

---
+++ RP2T3/src/repositorio/casa/Casa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repositorio.casa;
+
+/**
+ *
+ * @author Amanda Gobus
+ */
+public class Casa {
+
+    private int numeroquartos;
+    private double area;
+    private int vagasgaragem;
+    private int ano;
+
+}
