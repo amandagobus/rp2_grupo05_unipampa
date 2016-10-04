125591e11402c598eb40732934abfe73e88b22f6
amandagobus <amandagobus@gmai.com>
2016-10-04 20:08:23 -0300

    Alterações na Classe Apartamento (do pacote repositorio.apartamento) e exclusão do pacote Apartamento.java

--- RP2T3/src/repositorio/apartamento/Apartamento.java
+++ RP2T3/src/repositorio/apartamento/Apartamento.java
@@ -15,40 +15,77 @@ public class Apartamento extends Predio {
 
     public Apartamento(String logradouro, int numero, String bairro, String cidade, String descricao, double areaTotal, double valor, String NomeEdifico, int andar, double valorCondominio) {
         super(logradouro, numero, bairro, cidade, descricao, areaTotal, valor, NomeEdifico, andar, valorCondominio);
+
     }
 
+    /**
+     *
+     * @return NumeroQuartos
+     */
     public int getNumeroQuartos() {
         return numeroQuartos;
     }
 
+    /**
+     *
+     * @param numeroQuartos
+     */
     public void setNumeroQuartos(int numeroQuartos) {
         this.numeroQuartos = numeroQuartos;
     }
 
+    /**
+     *
+     * @return Numero de Vagas
+     */
     public int getNumeroVagas() {
         return numeroVagas;
     }
 
+    /**
+     *
+     * @param numeroVagas
+     */
     public void setNumeroVagas(int numeroVagas) {
         this.numeroVagas = numeroVagas;
     }
 
+    /**
+     *
+     * @return Ano
+     */
     public int getAno() {
         return ano;
     }
 
+    /**
+     *
+     * @param ano
+     */
     public void setAno(int ano) {
         this.ano = ano;
     }
 
+    /**
+     *
+     * @return Numero do Apartamento
+     */
     public int getNumeroDoApartamento() {
         return numeroDoApartamento;
     }
 
+    /**
+     *
+     * @param numeroDoApartamento
+     */
     public void setNumeroDoApartamento(int numeroDoApartamento) {
         this.numeroDoApartamento = numeroDoApartamento;
     }
 
+    /**
+     *
+     * @return dados
+     */
     @Override
     public String toString() {
         String dados = "";
