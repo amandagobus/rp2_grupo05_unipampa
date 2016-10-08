8c18ce7014fb3ba2511e797e45a997fc785a28a7
GuilhermeSSantos <guilthys@gmail.com>
2016-10-07 23:07:05 -0300

    Refatorado, falta menu Chacara e menu terreno

--- RP2T3/src/Main/RP2T3.java
+++ RP2T3/src/Main/RP2T3.java
@@ -5,20 +5,58 @@
  */
 package Main;
 
+import Menu.MenuApartamento;
+import Menu.MenuCasa;
 import Menu.MenuSalaComercial;
+import java.util.Scanner;
 
 /**
  *
  * @author Arcano
  */
 public class RP2T3 {
-    
-    
-    
+
     public static void main(String[] args) {
-        
-        MenuSalaComercial m = new MenuSalaComercial();
-        m.menuInicial();
+
+        MenuSalaComercial salaC = new MenuSalaComercial();
+        MenuApartamento Ap = new MenuApartamento();
+        MenuCasa casa = new MenuCasa();
+        int op = 1;
+        Scanner entrada = new Scanner(System.in);
+
+        while (op != 0) {
+            System.out.println(" ");
+            System.out.println("******** BEM VINDO AO MENU ********");
+            System.out.println("0) Sair\n1) Resumo\n2) Palestras\n3) Mini Curso\n"
+                    + "4) Artigo \n5) Monografia");
+            System.out.println(" ");
+            System.out.print("Opção: ");
+            op = entrada.nextInt();
+            entrada.hasNextLine();
+
+            switch (op) {
+                case (1):
+                    System.out.println(" ");
+                    Ap.menuInicial();
+
+                    break;
+
+                case (2):
+                    System.out.println(" ");
+                    casa.menuInicial();
+                    break;
+                case (3):
+                    System.out.println(" ");
+                    //chacara.menuInicial();
+                    break;
+                case (4):
+                    System.out.println(" ");
+                    salaC.menuInicial();
+                    break;
+                case (5):
+                    // terreno.MenuInicial();
+                    break;
+            }
+        }
     }
-    
 }
