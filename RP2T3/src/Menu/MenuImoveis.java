0e2fec23833ec725986d171f91c28ee04b7d3601
GuilhermeSSantos <guilthys@gmail.com>
2016-10-07 23:32:17 -0300

    Usar herança nos menus posteriormente

--- RP2T3/src/Menu/MenuImoveis.java
+++ RP2T3/src/Menu/MenuImoveis.java
@@ -5,50 +5,12 @@
  */
 package Menu;
 
-import Imovel.Residencia;
-import ListaImoveis.ListaDeImoveis;
-import Chacara.Chacara;
-import java.util.Scanner;
+
 
 /**
  *
  * @author Arcano
  */
 public class MenuImoveis {
-
-    ListaDeImoveis lista = new ListaDeImoveis();
-    Scanner imput = new Scanner(System.in);
-
-    public static void menu() {
-        System.out.println(" \n");
-        System.out.println("***** Menu *****\n");
-        System.out.println("1) Novo Imóvel ");
-        System.out.println("2) Consultar");
-        System.out.println("3) Editar ");
-        System.out.println("4) Excluir");
-        System.out.println("0) Voltar ");
-        System.out.println(" ");
-        System.out.print("Opção:    ");
-
-    }
-
-    public static void menu2() {
-        System.out.println(" \n");
-        System.out.println("*************** Menu de Consulta *****************");
-        System.out.println(" \n");
-        System.out.println("1) Código  ");
-        System.out.println("0) Voltar ");
-        System.out.println("\n ");
-        System.out.print("Opcao:     ");
-
-    }
-
-    String logradouro;
-    int numero;
-    String bairro;
-    String cidade;
-    String descricao;
-    double areaTotal;
-    double valor;
 
 }
