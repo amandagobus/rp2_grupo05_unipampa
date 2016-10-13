b6a7331933611a7ed6170bfa39b6596e9a22ec42
GuilhermeSSantos <guilthys@gmail.com>
2016-10-12 21:48:25 -0300

    Metodo editar, adcionado o index de onde adicionar o elemento.

--- RP2T3/src/ListaImoveis/ListaDeImoveis.java
+++ RP2T3/src/ListaImoveis/ListaDeImoveis.java
@@ -21,7 +21,7 @@ import java.io.OutputStreamWriter;
  */
 public class ListaDeImoveis implements ListaImoveis {
 
-     List<Imovel> lista = new ArrayList<>();
+    List<Imovel> lista = new ArrayList<>();
 
     @Override
     public boolean incluir(Imovel im) {
@@ -36,33 +36,33 @@ public class ListaDeImoveis implements ListaImoveis {
             if (codigo == imovel.getCodigo()) {
                 return imovel;
             }
-
         }
-
         return null;
 
     }
-
-    @Override
-    public boolean editar(int codigo, Imovel im) {
-      boolean  objeto; 
-    objeto = excluir(codigo);
-    if(objeto == true){
-    lista.add(im);
-    return true;
-     }else {
-    return false;}
+    
+      @Override
+    public boolean editar(int codigo, Imovel imo) {
+        boolean objeto;
+        int i = lista.indexOf(codigo);
+        objeto = excluir(codigo);
+        if (objeto == true) {
+            lista.add(i,imo);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public boolean excluir(int codigo) {
-    Imovel imo = consultar(codigo);    
-    if(imo != null){
-    lista.remove(imo);
-    return true;
-    } else{
-    return false;
-     }
+        Imovel imo = consultar(codigo);
+        if (imo != null) {
+            lista.remove(imo);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
@@ -100,23 +100,22 @@ public class ListaDeImoveis implements ListaImoveis {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void gravar() throws Exception{
-    FileWriter outFile = new FileWriter(new File("C:/Users/Arcano/Desktop/novo.CSV"));
-    BufferedWriter  escrever = new  BufferedWriter(outFile); 
-    
-     escrever.write("CODIGO,LOGRADOURO,NÚMERO,BAIRRO,CIDADE,DESCRIÇÃO,AREA TOTAL,VALOR,NOME DO EDIFICIO,ANDAR,VALOR DO CONDOMINIO,"
-             + "NÚMERO DE SALAS,NÚMERO DE BANHEIROS\r\n");
-        for (Imovel imovel : lista) {         
-            escrever.write(imovel.getCodigo()+","+imovel.getLogradouro()+","+imovel.getNumero()
-                    +","+imovel.getBairro()+","+imovel.getCidade()+","+imovel.getDescricao()+","+imovel.getAreaTotal()
-                    +","+imovel.getValor()+","+imovel.getNomeEdificio()+","+imovel.getAndar()+","+imovel.getValorCondominio()
-                    +","+imovel.getNumeroDaSala()+","+imovel.getNumeroDeBanheiros());  
+    public void gravar() throws Exception {
+        FileWriter outFile = new FileWriter(new File("C:/Users/Arcano/Desktop/novo.CSV"));
+        BufferedWriter escrever = new BufferedWriter(outFile);
+
+        escrever.write("CODIGO,LOGRADOURO,NÚMERO,BAIRRO,CIDADE,DESCRIÇÃO,AREA TOTAL,VALOR,NOME DO EDIFICIO,ANDAR,VALOR DO CONDOMINIO,"
+                + "NÚMERO DE SALAS,NÚMERO DE BANHEIROS\r\n");
+        for (Imovel imovel : lista) {
+            escrever.write(imovel.getCodigo() + "," + imovel.getLogradouro() + "," + imovel.getNumero()
+                    + "," + imovel.getBairro() + "," + imovel.getCidade() + "," + imovel.getDescricao() + "," + imovel.getAreaTotal()
+                    + "," + imovel.getValor() + "," + imovel.getNomeEdificio() + "," + imovel.getAndar() + "," + imovel.getValorCondominio()
+                    + "," + imovel.getNumeroDaSala() + "," + imovel.getNumeroDeBanheiros());
             escrever.write("\r\n");
-            
-           
+
         }
         escrever.close();
         outFile.close();
-       
+
     }
 }
