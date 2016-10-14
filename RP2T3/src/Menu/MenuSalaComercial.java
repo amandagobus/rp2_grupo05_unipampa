afc5fcfb6787aed239f417a46afc0202eb26e24e
GuilhermeSSantos <guilthys@gmail.com>
2016-10-14 00:45:50 -0300

    Metodo Editar parcialmente completo

--- RP2T3/src/Menu/MenuSalaComercial.java
+++ RP2T3/src/Menu/MenuSalaComercial.java
@@ -9,6 +9,8 @@ import Imovel.Imovel;
 import ListaImoveis.ListaDeImoveis;
 import SalaComercial.SalaComercial;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -47,7 +49,7 @@ public class MenuSalaComercial {
     }
 
     /**
-     * Metodo que faz a interação com o usuário, passando após as informações 
+     * Metodo que faz a interação com o usuário, passando após as informações
      * para o construtor.
      */
     public void IncluirImovel() {
@@ -97,7 +99,7 @@ public class MenuSalaComercial {
         System.out.print("Digite o Valor do Condominio:  ");
         valorCondominio = entrada.nextDouble();
         entrada.nextLine();
-        
+
         System.out.print("Digite o Número da Sala:  ");
         numeroDaSala = entrada.nextInt();
         entrada.nextLine();
@@ -111,12 +113,11 @@ public class MenuSalaComercial {
                 numeroDeBanheiros, numeroDaSala);
 
         boolean objeto = lista.incluir(salaC);
-     
-        
+
         System.out.println("\n\n");
-        if(objeto == true){
+        if (objeto == true) {
             System.out.println("Imóvel incluido com sucesso.");
-        }else{
+        } else {
             System.out.println("Imóvel não foi incluido.");
         }
         try {
@@ -144,17 +145,157 @@ public class MenuSalaComercial {
         }
 
     }
-    
-    public void Carregar(){
-    
-        try{        
-        lista.ler();       
-        }catch(Exception e){}
-        
-    
+
+    public void Carregar() {
+
+        try {
+            lista.ler();
+        } catch (Exception e) {
+        }
+
     }
-    
-    
+
+    public void editarControle() {
+        
+        System.out.println("\n");
+        System.out.println("***********  Menu Editar ************ ");
+        System.out.println("\n ");
+        System.out.println("Digite Titulo Que Deseja  Editar:");
+        int codigoConsulta = entrada.nextInt();
+        entrada.nextLine();
+        Imovel editarLista = lista.consultar(codigoConsulta);
+        entrada.nextLine();
+
+        
+        int editar = 0;
+
+        if (editarLista != null) {
+            do {
+                System.out.println(" \n");
+                System.out.println("Escolha a Opção a Ser Editada:");
+                System.out.println(" \n");
+                System.out.println("1) Titulo ");
+                System.out.println("2) Autores");
+                System.out.println("3) Instituição ");
+                System.out.println("4) Palavras Chave ");
+                System.out.println("5) Situação");
+                System.out.println("0) Voltar ");
+                System.out.println(" ");
+                System.out.print("Opção:    ");
+                editar = entrada.nextInt();
+                entrada.nextLine();
+
+                switch (editar) {
+                    case 1:
+                        System.out.println("\n ");
+                        System.out.println("DIGITE O NOVO LOGRADOURO: ");
+                        editarLista.setLogradouro(entrada.nextLine());
+                        break;
+                    case 2:
+
+                        System.out.println("\n\n");
+                        System.out.println("DIGITE O NÚEMRO:");
+                        editarLista.setNumero(entrada.nextInt());
+                        entrada.nextLine();
+
+                        break;
+
+                    case 3:
+
+                        System.out.println("\n");
+                        System.out.println("DIGITE O BAIRRO:");
+                        editarLista.setBairro(entrada.nextLine());
+
+                        break;
+
+                    case 4:
+
+                        System.out.println("\n");
+                        System.out.println("DIGITE A CIDADE:");
+                        editarLista.setCidade(entrada.nextLine());
+
+                        break;
+
+                    case 5:
+
+                        System.out.println("\n");
+                        System.out.println("DIGITE A DESCRIÇÂO:");
+                        editarLista.setCidade(entrada.nextLine());
+
+                        break;
+
+                    case 6:
+
+                        System.out.println("\n");
+                        System.out.println("DIGITE A ARÉA TOTAL:");
+                        editarLista.setAreaTotal(entrada.nextDouble());
+                        entrada.nextLine();
+
+                        break;
+                     
+                    case 7: 
+                        System.out.println("\n");
+                        System.out.println("DIGITE O VALOR:");
+                        editarLista.setValor(entrada.nextDouble());
+                        
+                        break;
+                        
+                    case 8: 
+                        System.out.println("\n");
+                        System.out.println("DIGITE O NOME DO EDIFICIO:");
+                        editarLista.setNomeEdificio(entrada.nextLine());
+                        
+                        break;
+                        
+                    case 9:
+                        
+                        System.out.println("\n");
+                        System.out.println("DIGITE O ANDAR:");
+                        editarLista.setAndar(entrada.nextInt());
+                        entrada.nextLine();
+                        break;
+                        
+                        
+                    case 10:
+                        
+                        System.out.println("\n");
+                        System.out.println("DIGITE O VALOR DO CONDOMINIO:");
+                        editarLista.setValor(entrada.nextDouble());
+                        entrada.nextLine();
+                        break;
+                        
+                        
+                    case 11:
+                        
+                        System.out.println("\n");
+                        System.out.println("DIGITE O NÚMERO DA SALA:");
+                        editarLista.setNumeroDaSala(entrada.nextInt());
+                        entrada.nextLine();
+                        
+                        break;
+                        
+                        
+                    case 12:
+                        
+                        System.out.println("\n");
+                        System.out.println("DIGITE O NÚMERO DE BANHEIRO:");
+                        editarLista.setNumeroDeBanheiros(entrada.nextInt());
+                        entrada.nextLine();
+                        
+                        break;
+ 
+                }
+
+            } while (editar != 0);
+
+            lista.editar(codigoConsulta, editarLista);
+
+        } else {
+            System.out.println(" \nResumo Não Encotrado ");
+        }
+
+    }
+
     public void menuInicial() {
         int i;
         Carregar();
