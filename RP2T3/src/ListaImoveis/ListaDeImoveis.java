2e8f415f63e9efad6b52ffd3770a171b2718b778
GuilhermeSSantos <guilthys@gmail.com>
2016-10-14 13:52:18 -0300

    Metodo Editar algumas mudanças, sem validação

--- RP2T3/src/ListaImoveis/ListaDeImoveis.java
+++ RP2T3/src/ListaImoveis/ListaDeImoveis.java
@@ -10,15 +10,10 @@ import SalaComercial.SalaComercial;
 import java.util.ArrayList;
 import java.util.List;
 import Imovel.ListaImoveis;
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 /**
@@ -107,11 +102,20 @@ public class ListaDeImoveis implements ListaImoveis {
     }
 
     public void gravar() throws Exception {
+        
+        
+        //verificar se o arquivo existe, se não existeir criar (o ato de recriar o mesmo arquivo ja resolve por se só?)
+        
+        
         FileWriter outFile = new FileWriter(new File("C:/Users/Arcano/Desktop/novo.CSV"));
         BufferedWriter escrever = new BufferedWriter(outFile);
 
         escrever.write("CODIGO,LOGRADOURO,NÚMERO,BAIRRO,CIDADE,DESCRIÇÃO,AREA TOTAL,VALOR,NOME DO EDIFICIO,ANDAR,VALOR DO CONDOMINIO,"
                 + "NÚMERO DE SALAS,NÚMERO DE BANHEIROS\r\n");
+        
+        // criar um metodo nas class abstratas Filewhite. 
+        
+        
         for (Imovel imovel : lista) {
             escrever.write(imovel.getCodigo() + "," + imovel.getLogradouro() + "," + imovel.getNumero()
                     + "," + imovel.getBairro() + "," + imovel.getCidade() + "," + imovel.getDescricao() + "," + imovel.getAreaTotal()
@@ -126,7 +130,13 @@ public class ListaDeImoveis implements ListaImoveis {
     }
 
     public void ler() throws Exception {
-
+        
+        
+        
+        //verificar se o arquivo existe, antes da leitura.
+        
+        
+        
         String logradouro, bairro, cidade, descricao, nomeEdificio;
         int codigo, numero, andar, numeroSala, NumeroBanheiro;
         double areaTotal, valor, valorCondominio;
